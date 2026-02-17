295ba7d905e633ad3f7588e62dd508705a698eaa
vmassonniere <vmassonniere@icebergdatalab.com>
2026-02-17 21:10:35 +0100

    first commit

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,10 +4,10 @@ return {
     opts = {
       style = "night",
       on_colors = function(colors)
-        colors.bg = "#0a0b10"
-        colors.bg_dark = "#06070b"
-        colors.bg_float = "#0a0b10"
-        colors.bg_sidebar = "#06070b"
+        colors.bg = "#151821"
+        colors.bg_dark = "#11141b"
+        colors.bg_float = "#151821"
+        colors.bg_sidebar = "#11141b"
       end,
     },
   },
