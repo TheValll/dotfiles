2ffeee4f0a97aefff20c76b39e101072c85b565f
vmassonniere <vmassonniere@icebergdatalab.com>
2026-02-17 21:06:38 +0100

    first commit

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,12 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       style = "night",
+      on_colors = function(colors)
+        colors.bg = "#0a0b10"
+        colors.bg_dark = "#06070b"
+        colors.bg_float = "#0a0b10"
+        colors.bg_sidebar = "#06070b"
+      end,
     },
   },
 }
